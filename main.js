35c0f8631ba83dacbfc332769d685ca7df90531f
camilohm1 <cahernandez3510@misena.edu.co>
2022-11-08 11:02:29 -0500

    punto 4

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-addEventListener("DOMContentLoaded",(e)=>{
+addEventListener("DOMContentLoaded", (e) => {
 
-
-
-    let n = document.querySelector("#N")
-    let resultado = ""
-    let numeros = document.querySelector("#numeros")
-    let suma = 0
-    let res= document.querySelector("#resultados")
+    let primera = document.querySelector("#ventas");
+    let resultados = document.querySelector("#resultados");
+    let Venta1 = document.querySelector("#Venta1");
+    let Venta2 = document.querySelector("#Venta2");
+    let Venta3 = document.querySelector("#Venta3");
+    let suldoB = 1000000
+   
+    ventas.addEventListener("submit", (e) => {
+   
+     e.preventDefault();
+   
+    let dataInput = Object.fromEntries(new FormData(e.target));
     
-    numeros.addEventListener("submit",(e)=>{
-        e.preventDefault();
-        let datosEntrada = Object.fromEntries(new FormData(e.target));
-        for (let s = 1; s < datosEntrada.N; s++){
-            if(datosEntrada.N % s == 0){
-                suma+=s
-            }
-        }
-        if(datosEntrada.N == suma){
-            resultado = "Es perfecto"
-            suma = 0
-        }
-        else{
-            resultado = "No es perfecto"
-            suma = 0
-        }
-        res.innerHTML = ""
-        res.insertAdjacentHTML("beforeend",`
-        <h2> ${datosEntrada.N + ' ' + resultado} </h2>
-        `);
-        })
+    let Comision1 = Number(dataInput.Venta1) * 0.10
+    let Comision2 = Number(dataInput.Venta2) * 0.10
+    let Comision3 = Number(dataInput.Venta3) * 0.10
+    
+    let Comision = Comision1 + Comision2 + Comision3
+    let total = suldoB + Comision
+    
+    resultados.innerHTML = total
     })
+    
+   })
+   
